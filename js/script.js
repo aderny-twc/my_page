4e80597f3b43e4b2ef644b6a491d89cf1a41139e
Andrey Belskiy <diksandab@gmail.com>
2021-04-27 15:11:13 +0600

    Created navbar. Added background placeholder. Added function for navbar

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// STICKY NAVBAR
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function stickyNavScroll() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+window.onscroll = function() {stickyNavScroll()};
